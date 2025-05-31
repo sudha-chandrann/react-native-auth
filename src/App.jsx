4d9f4896a1ca5ec8c3ecc444c864cb44535d207a
sudhachandrann <sudhachandran1020@gmail.com>
2025-06-01 00:51:45 +0530

    initial init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/lib/constants/appwrite.js
@@ -0,0 +1,1 @@
+export const APPWRITE_PROJECT_ID = '';
